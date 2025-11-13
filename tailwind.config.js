b33f87c82bcb7584ea4a78085a96100d3835ffb7
zieeco <zieecosam@gmail.com>
2025-11-13 15:17:32 +0100

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,9 @@ module.exports = {
 	darkMode: ['class'],
 	content: [
 		'./index.html',
-		'./src/**/*.{js,ts,jsx,tsx}',
-		'./src/components/**/*.{js,ts,jsx,tsx}',
-		'./src/features/**/*.{js,ts,jsx,tsx}',
-		'./src/layouts/**/*.{js,ts,jsx,tsx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
