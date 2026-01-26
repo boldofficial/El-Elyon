2eb9ad22753106e5786f0d370814763097fd4fb6
zieeco <zieecosam@gmail.com>
2026-01-26 11:16:06 +0100

    feat: add web analytics to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import { Analytics } from '@vercel/analytics/next';
 import {ClerkProvider} from '@clerk/nextjs';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
@@ -45,6 +46,7 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 					{children}
+					<Analytics />
 				</body>
 			</html>
 		</ClerkProvider>
